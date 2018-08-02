b577d65bbea12e017daf619935667ef969394c7c
pynoor <32771762+pynoor@users.noreply.github.com>
2018-08-02 20:03:56 +0200

    Add image to instruction

--- tests.py
+++ tests.py
@@ -4,24 +4,29 @@ from cell import Cell
 
 class BoardTestCase(unittest.TestCase):
     def setUp(self):
-        self.FirstBoard = Board([['+', '-', '-', '+'],
+        self.first_board = Board([['+', '-', '-', '+'],
                             ['+', '+', '+', '+'],
                             ['-', '-', '+', '-'],
                             ['+', '-', '-', '-']])
-        self.SecondBoard = Board([['-', '-', '+', '-'],
+        self.second_board = Board([['-', '-', '+', '-'],
                             ['-','+', '+', '+'],
                             ['+', '-', '+', '-']
                             ])
 
 
-    def test_board_sets_correct_neighbours(self):
-        self.FirstBoard.set_neighbours()
-        result = self.FirstBoard.cells[1][2].neighbour_states
+    def test_board_sets_neighbours(self):
+        # Exercise
+        self.first_board.set_neighbours()
+
+        # Verify
+        random_cell = self.first_board.cells[1][2]
+        result = random_cell.neighbour_states
         expected_result = ('+', '+', '-', '+', '-', '-', '-', '+')
         self.assertCountEqual(result, expected_result)
 
-    def test_board_generates_correct_new_board(self):
-        result = self.SecondBoard.generate_new_board()
+    def test_board_generates_new_board(self):
+        result = self.second_board.generate_new_board()
+
         expected_result = [['-', '+', '+', '+'],
                             ['-', '-', '-', '+'],
                             ['-', '-', '+', '+'],
@@ -31,16 +36,16 @@ class BoardTestCase(unittest.TestCase):
 
 class CellTestCase(unittest.TestCase):
     def setUp(self):
-        self.cell_1 = Cell('+', ('-', '+', '+'))
-        self.cell_2 = Cell('-', ('+', '+', '+', '-', '-'))
+        self.alive_cell = Cell('+', ('-', '+', '+'))
+        self.dead_cell = Cell('-', ('+', '+', '+', '-', '-'))
 
     def test_alive_cell_with_two_alive_neighbours_stays_alive(self):
-        self.cell_1.update_state()
-        self.assertEqual(self.cell_1.next_state, '+')
+        self.alive_cell.update_state()
+        self.assertEqual(self.alive_cell.next_state, '+')
 
     def test_dead_cell_with_three_alive_neighbours_is_revived(self):
-        self.cell_2.update_state()
-        self.assertEqual(self.cell_2.next_state, '+')
+        self.dead_cell.update_state()
+        self.assertEqual(self.dead_cell.next_state, '+')
 
 if __name__ == '__main__':
     unittest.main()
