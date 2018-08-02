789d7d03526b163dd7c777fa13999648ca193d0f
pynoor <32771762+pynoor@users.noreply.github.com>
2018-08-02 16:06:00 +0200

    Refactor Board structure

--- tests.py
+++ tests.py
@@ -4,15 +4,16 @@ from cell import Cell
 
 class BoardTestCase(unittest.TestCase):
     def setUp(self):
-        self.TestBoard = Board([4, 4], ['+', '-', '-', '+',
-                            '+', '+', '+', '+',
-                            '-', '-', '+', '-',
-                            '+', '-', '-', '-'])
+        self.TestBoard = Board([['+', '-', '-', '+'],
+                            ['+', '+', '+', '+'],
+                            ['-', '-', '+', '-'],
+                            ['+', '-', '-', '-']])
 
     def test_board_sets_correct_neighbours(self):
-        result = [set([neighbour.state for neighbour in self.TestBoard.cells[0].neighbours]),
-         set([neighbour.state for neighbour in self.TestBoard.cells[3].neighbours])]
-        expected_result = [('-', '+', '+'), ('-', '+', '+')]
+        self.TestBoard.set_neighbours()
+        result = self.TestBoard.cells[0].neighbours
+        expected_result = ('-', '+', '+')
         self.assertEqual(result, expected_result)
+
 if __name__ == '__main__':
     unittest.main()
