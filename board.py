6ab5cc38a9c91d895f17bee03bbeb33116559e61
pynoor <32771762+pynoor@users.noreply.github.com>
2018-08-02 17:20:36 +0200

    Set up set_neighbours function - GREEN

--- board.py
+++ board.py
@@ -1,11 +1,92 @@
+from cell import Cell
+
 class Board:
 
-    def __init__(self, list_of_cells):
+    def __init__(self, list_of_cell_state_rows):
+        self.list_of_cell_state_rows = list_of_cell_state_rows
         self.cells = []
-        self.dimensions = []
+
+        for cell_state_row in self.list_of_cell_state_rows:
+            new_cell_row = []
+            for cell_state in cell_state_row:
+                new_cell = Cell(cell_state)
+                new_cell_row.append(new_cell)
+            self.cells.append(new_cell_row)
+        self.set_neighbours()
+
 
     def set_neighbours(self):
-        pass
+        for cell_row in self.cells:
+            for cell in cell_row:
+                cell_index = cell_row.index(cell)
+                cell_row_index = self.cells.index(cell_row)
+                if cell_index == 0:
+                    if cell_row_index == 0:
+                        cell.neighbour_states = (
+                            self.list_of_cell_state_rows[0][1],
+                            self.list_of_cell_state_rows[1][0],
+                            self.list_of_cell_state_rows[1][1],
+                            )
+                    elif cell_row_index != len(self.list_of_cell_state_rows) - 1:
+                        cell.neighbour_states = (
+                            self.list_of_cell_state_rows[cell_row_index-1][0],
+                            self.list_of_cell_state_rows[cell_row_index-1][1],
+                            self.list_of_cell_state_rows[cell_row_index][1],
+                            self.list_of_cell_state_rows[cell_row_index+1][0],
+                            self.list_of_cell_state_rows[cell_row_index+1][1]
+                        )
+                    else:
+                        cell.neighbour_states = (
+                            self.list_of_cell_state_rows[cell_row_index-1][0],
+                            self.list_of_cell_state_rows[cell_row_index-1][1],
+                            self.list_of_cell_state_rows[cell_row_index][1]
+                        )
+                elif cell_index == len(cell_row) - 1:
+                    if cell_row_index == 0:
+                        cell.neighbour_states = (
+                            self.list_of_cell_state_rows[0][cell_index-1], self.list_of_cell_state_rows[1][cell_index],
+                            self.list_of_cell_state_rows[1],[cell_index-1]
+                            )
+                    elif cell_row_index != len(self.list_of_cell_state_rows) - 1:
+                        cell.neighbour_states = (
+                            self.list_of_cell_state_rows[cell_row_index-1][cell_index-1],
+                            self.list_of_cell_state_rows[cell_row_index-1][cell_index],
+                            self.list_of_cell_state_rows[cell_row_index][cell_index-1],
+                            self.list_of_cell_state_rows[cell_row_index+1][cell_index],
+                            self.list_of_cell_state_rows[cell_row_index+1][cell_index-1]
+                        )
+                    else:
+                        cell.neighbour_states = (
+                            self.list_of_cell_state_rows[cell_row_index-1][cell_index-1],
+                            self.list_of_cell_state_rows[cell_row_index-1][cell_index],
+                            self.list_of_cell_state_rows[cell_row_index][cell_index-1]
+                        )
+                else:
+                    if cell_row_index == 0:
+                        cell.neighbour_states = (
+                            self.list_of_cell_state_rows[0][cell_index-1],
+                            self.list_of_cell_state_rows[0][cell_index+1], self.list_of_cell_state_rows[1][cell_index],
+                            self.list_of_cell_state_rows[1][cell_index+1],
+                            self.list_of_cell_state_rows[1],[cell_index-1],
+                            )
+                    elif cell_row_index != len(self.list_of_cell_state_rows) - 1:
+                        cell.neighbour_states = (
+                            self.list_of_cell_state_rows[cell_row_index-1][cell_index-1],
+                            self.list_of_cell_state_rows[cell_row_index-1][cell_index],
+                            self.list_of_cell_state_rows[cell_row_index-1][cell_index+1],
+                            self.list_of_cell_state_rows[cell_row_index][cell_index+1],
+                            self.list_of_cell_state_rows[cell_row_index][cell_index-1],
+                            self.list_of_cell_state_rows[cell_row_index+1][cell_index-1],
+                            self.list_of_cell_state_rows[cell_row_index+1][cell_index],
+                            self.list_of_cell_state_rows[cell_row_index+1][cell_index+1]
+                        )
+                    else:
+                        cell.neighbour_states = (
+                            self.list_of_cell_state_rows[cell_row_index-1][cell_index-1],
+                            self.list_of_cell_state_rows[cell_row_index-1][cell_index+1], self.list_of_cell_state_rows[cell_row_index-1][cell_index],
+                            self.list_of_cell_state_rows[cell_row_index][cell_index+1],
+                            self.list_of_cell_state_rows[cell_row_index],[cell_index-1]
+                            )
 
     def set_new_states(self):
         pass
