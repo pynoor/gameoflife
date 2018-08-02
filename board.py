e3e7b3fed305b4814ec3f2cc576ae901bb80132e
pynoor <32771762+pynoor@users.noreply.github.com>
2018-08-02 15:27:30 +0200

    Add board class

---
+++ board.py
@@ -0,0 +1,13 @@
+class Board:
+
+    def __init__(self, list_of_cells):
+        self.cells = []
+
+    def set_neighbours(self):
+        pass
+
+    def set_new_states(self):
+        pass
+
+    def generate_new_board(self):
+        pass
