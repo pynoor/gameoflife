d17c240fe1502db6f98fa8e72992601595e07a5e
pynoor <32771762+pynoor@users.noreply.github.com>
2018-08-02 17:31:16 +0200

    Set up update state function - GREEN

--- cell.py
+++ cell.py
@@ -1,11 +1,15 @@
 class Cell:
 
-    def __init__(self, state, neighbours):
+    def __init__(self, state, neighbour_states = ()):
         self.state = state
-        self.neighbours = []
-
-    def request_new_state(self):
-        pass
+        self.neighbour_states = neighbour_states
 
     def update_state(self):
-        self.state = self.request_new_state()
+        alive_neighbour_count = list(self.neighbour_states).count('+')
+        if self.state == '+':
+            if alive_neighbour_count < 2 or alive_neighbour_count > 3:
+                self.state = '-'
+            elif alive_neighbour_count in (2, 3):
+                return
+        elif self.state == '-' and alive_neighbour_count == 3:
+            self.state = '+'
