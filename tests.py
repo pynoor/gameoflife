fda7c3d5414597eaeb3cd21997f265a83758fa64
pynoor <32771762+pynoor@users.noreply.github.com>
2018-08-02 17:25:22 +0200

    Add test for cell state update

--- tests.py
+++ tests.py
@@ -11,9 +11,19 @@ class BoardTestCase(unittest.TestCase):
 
     def test_board_sets_correct_neighbours(self):
         self.TestBoard.set_neighbours()
-        result = self.TestBoard.cells[0].neighbours
+        result = self.TestBoard.cells[0][0].neighbour_states
         expected_result = ('-', '+', '+')
         self.assertEqual(result, expected_result)
+
+class CellTestCase(unittest.TestCase):
+    def setUp(self):
+        self.cell = Cell('+', ('-', '+', '+'))
+
+    def test_alive_cell_with_two_alive_neighbours_stays_alive(self):
+        self.cell.update_state()
+        self.assertEqual(self.cell.state, '+')
+
+
 
 if __name__ == '__main__':
     unittest.main()
